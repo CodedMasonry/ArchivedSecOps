69c875aa3395e2c5d74f11b35a18430a0586d748
Mason <109301662+CodedMasonry@users.noreply.github.com>
2024-01-01 11:21:32 -0500

    Add subdomain module
    Add the subdomain module and required types

---
+++ tricoder/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod model;
+pub mod subdomains;
+
+use thiserror::Error;
+
+#[derive(Error, Debug, Clone)]
+pub enum Error {
+    #[error("Usage: Tricoder <kerkour.com>")]
+    CliUsage,
+    
+    #[error("Reqwest: {0}")]
+    Reqwest(String),
+}
+
+impl std::convert::From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Error::Reqwest(err.to_string())
+    }
+}
--- tricoder/src/main.rs
+++ tricoder/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use tricoder::subdomains::enumerate;
+
+fn main() -> Result<(), anyhow::Error> {
+    let client = reqwest::blocking::Client::new();
+    let result = enumerate(&client, "google.com")?;
+
+    println!("[+] {:#?}", result);
+
+    Ok(())
 }
